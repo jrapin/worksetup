1bff351d0613f5c66f0e50f890644c766541f286
Jeremy <jeremy@cardiologs.com>
2018-04-13 18:11:14 +0200

    Added tests

--- configfiles/install.py
+++ configfiles/install.py
@@ -2,32 +2,47 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 from pathlib import Path
 
 
-def symlink_to_home(files_directory, backup_directory):
-    files_directory = Path(files_directory).absolute()
-    for subpath in files_directory.iterdir():
-        filepath = files_directory / subpath.name
-        homefilepath = Path.home() / ("." + subpath.name)
+def symlink_to_home(backup_dir, home_dir=None):
+    """Creates symlinks of the configuration files in the
+    files directory to the home directory.
+
+    Parameters
+    ----------
+    backup_dir: str or Path
+        path of the directory into which existing configuration files
+        should be backed-up.
+    home_dir: str or Path
+        path of the directory into which the files must be symlinked.
+
+    Note
+    ----
+    Current configuration files are flake8, pylintrc, vimrc and a "myconfig"
+    file which should be sourced in the bashrc or zshrc.
+    """
+    home_dir =  Path.home() if home_dir is None else Path(home_dir)
+    files_dir = Path(__file__).absolute().parent / "files"
+    backup_dir = Path(backup_dir).absolute()
+    for subdir in files_dir.iterdir():
+        new_file = files_dir / subdir.name
+        home_file = home_dir / ("." + subdir.name)
         # create backup if already exists as real file
-        if homefilepath.is_file() and not homefilepath.is_symlink():
-            backup_directory.mkdir(exist_ok=True)
-            backupfilepath = backup_directory / subpath.name
-            if backupfilepath.exists():
-                #os.remove(str(backupfilepath))
-                backupfilepath.unlink()
-            shutil.move(str(homefilepath), str(backupfilepath))
+        if home_file.is_file() and not home_file.is_symlink():
+            backup_dir.mkdir(exist_ok=True)
+            backup_file = backup_dir / subdir.name
+            if backup_file.exists():
+                backup_file.unlink()
+            shutil.move(str(home_file), str(backup_file))
         # create symlink
-        if homefilepath.exists() or homefilepath.is_symlink():  # deal with broken links
-            #os.remove(homefilepath)
-            homefilepath.unlink()
-        homefilepath.symlink_to(filepath)
+        if home_file.is_file() or home_file.is_symlink():  # deal with broken links
+            home_file.unlink()
+        home_file.symlink_to(new_file)
 
 
 if __name__ == "__main__":
-    dirname = Path(__file__).parent
-    filedir = dirname / "files"
-    backupdir = dirname / "backup"
-    symlink_to_home(filedir, backupdir)
+    backup_dir = Path(__file__).parent / "backup"
+    symlink_to_home(backup_dir)
 
---
+++ configfiles/test_install.py
@@ -0,0 +1,48 @@
+import tempfile
+from pathlib import Path
+import install
+
+
+class TempConfigDirectories(tempfile.TemporaryDirectory):
+
+    def __enter__(self):
+        root_dir = Path(super().__enter__())
+        home_dir = root_dir / "home"
+        home_dir.mkdir()
+        backup_dir = root_dir / "backup"
+        return home_dir, backup_dir
+
+
+def test_context():
+    with TempConfigDirectories() as (home_dir, backup_dir):
+        assert home_dir.exists()
+    assert not home_dir.exists()
+
+
+def test_install_with_file():
+    with TempConfigDirectories() as (home_dir, backup_dir):
+        (home_dir / ".vimrc").touch()  # create a file to be backed up
+        install.symlink_to_home(backup_dir, home_dir=home_dir)
+        # check new files exist
+        names = {x.name for x in home_dir.iterdir()}
+        assert not names ^ {'.flake8', '.myconfig', '.pylintrc', '.vimrc'}
+        backed_up = {x.name for x in backup_dir.iterdir()}
+        assert not backed_up ^ {'vimrc'}
+
+
+def test_install_with_symlinks():
+    with TempConfigDirectories() as (home_dir, backup_dir):
+        install.symlink_to_home(backup_dir, home_dir=home_dir)
+        assert not backup_dir.exists()  # initially, empty dir
+        install.symlink_to_home(backup_dir, home_dir=home_dir)
+        assert not backup_dir.exists()  # symlinks should not be copied
+
+
+def test_install_with_dead_symlink():
+    with TempConfigDirectories() as (home_dir, backup_dir):
+        # create a dead symlink
+        vimrc = home_dir / ".vimrc"
+        vimrc.symlink_to(home_dir / "inexistant_file")
+        assert not vimrc.exists() and vimrc.is_symlink()  # dead symlink
+        install.symlink_to_home(backup_dir, home_dir=home_dir)
+
